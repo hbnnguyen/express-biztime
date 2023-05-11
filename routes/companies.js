7d1b14e5fed58f579fea7d71a05d37bf86f9f2d6
Hannah Nguyen <hnguyen160801@gmail.com>
2023-05-11 15:30:50 -0400

    add more routes

--- routes/companies.js
+++ routes/companies.js
@@ -12,10 +12,50 @@ const db = require("../db");
  */
 router.get("/", async function (req, res) {
   const results = await db.query("SELECT code, name FROM companies");
-  console.log(results);
   const companies = results.rows;
-  debugger
+
   return res.json({ companies });
 });
+
+
+/**
+ * TODO: write me
+ */
+router.get("/:code", async function (req, res) {
+  const code = req.params.code;
+
+  const results = await db.query(
+    `SELECT code, name, description
+    FROM companies
+    WHERE code = '${code}'`);
+
+  const company = results.rows[0];
+
+  // if (!company) throw new NotFoundError(`No matching company: ${code}`);
+  return res.json({ company });
+});
+
+
+/**
+ * TODO: write me
+ */
+router.post("/", async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
+  const { code, name, description } = req.body;
+
+  const results = await db.query(
+    `INSERT INTO companies
+    VALUES (${code}, ${name}, ${description})
+    RETURNING code, name, description`
+  );
+
+  const company = results.rows[0];
+
+  return res.status(201).json({ company });
+});
+
+
+
+
 
 module.exports = router;
