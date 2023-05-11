4742c82c8ed9a88821e54c36f742798c18f138cf
Jonathan Stern <jstern718@gmail.com>
2023-05-11 18:22:37 -0400

    debug error handling and post route

--- routes/companies.js
+++ routes/companies.js
@@ -43,13 +43,23 @@ router.post("/", async function (req, res) {
   if (req.body === undefined) throw new BadRequestError();
   const { code, name, description } = req.body;
 
+  const codeSearch = await db.query(
+    `SELECT code FROM companies
+    WHERE code = $1`, [code]
+  );
+  console.log("you made it");
+  if (codeSearch.rows[0]) {
+    throw new BadRequestError("This company already exists");
+  }
+  // console.log("you made it");
   const results = await db.query(
     `INSERT INTO companies
-    VALUES (${code}, ${name}, ${description})
+    VALUES ('${code}', '${name}', '${description}')
     RETURNING code, name, description`
   );
 
   const company = results.rows[0];
+  // console.log(results.rows)
 
   return res.status(201).json({ company });
 });
